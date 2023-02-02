83b3d5d0e21afd7172dd41a3eae31ef67e9086a7
DragStark <kdragstark@gmail.com>
2023-02-02 08:29:54 +0000

    Add avatar on user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     protected
             
     def configure_premitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :avatar])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :avatar])
     end
 end
