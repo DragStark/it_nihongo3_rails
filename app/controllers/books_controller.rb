73b523dc7835ce20a2fb67b0b4e23793f7e244b6
DragStark <kdragstark@gmail.com>
2023-02-02 09:22:18 +0000

    Add review book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,7 +24,7 @@ class BooksController < ApplicationController
   # POST /books or /books.json
   def create
     @book = Book.new(book_params)
-
+    
     respond_to do |format|
       if @book.save
         format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
