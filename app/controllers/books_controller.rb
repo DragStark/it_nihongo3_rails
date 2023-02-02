4d11edd2f373461d9eda4b5bb694b6ee09b7a436
DragStark <kdragstark@gmail.com>
2023-02-02 09:06:31 +0000

    Add review book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,10 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+
   # GET /books/1 or /books/1.json
   def show
+      @book_review = BookReview.new
   end
 
   # GET /books/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'user', to: "user#show"
   get 'static_pages/home'
+  
+  
   devise_for :users
+  get 'user', to: "user#show"
+  
   resources :books
   root "static_pages#home"
+  
+  resources :book_reviews
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
